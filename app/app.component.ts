2a2901878f1302635551ba54a592e04dbfaa7023
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-01-23 22:32:07 -0500

    added the hero interface and hero details to tpl

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string
+}
+
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.tpl.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
