16fcd8733e879643c6382e622af85421b0c1be5c
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-01-23 23:31:40 -0500

    added a master-detail view to the component

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,12 +7,28 @@ interface Hero {
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.tpl.html'
+    templateUrl: 'app/app.tpl.html',
+    styleUrls: ['app/my-app.css']
 })
 export class AppComponent {
     public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    public selectedHero: Hero;
+    public heroes = HEROES;
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
 }
+
+var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
