942b6db644b6ea067c91f93da290b002caff1574
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-01-24 19:59:41 -0500

    moved the hero detail directive to another file

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'my-hero-detail',
+    templateUrl: 'app/hero-detail.tpl.html',
+    inputs: ['hero']
+})
+export class HeroDetailComponent {
+
+}
